394d68c09550720e93786f5928c46c5bd62b1c63
邵庆玲 <shaoqingling@192.168.2.197>
2022-11-21 12:16:13 +0800

    commit

---
+++ src/tk/amrom/ArithmeticOpreation01.java
@@ -0,0 +1,21 @@
+package tk.amrom;
+
+import java.util.Scanner;
+
+public class ArithmeticOpreation01 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个整数");
+
+        int number = sc.nextInt();
+
+        int ge = number % 10;
+        int shi = number / 10 % 10;
+        int bai = number / 100 % 10;
+
+        System.out.println("个位是：" + ge);
+        System.out.println("十位是：" + shi);
+        System.out.println("百位是：" + bai);
+
+    }
+}
