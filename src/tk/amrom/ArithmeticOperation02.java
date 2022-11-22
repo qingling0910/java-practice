b004894805174d7c8986c624bbc8664777811f46
邵庆玲 <shaoqingling@192.168.2.197>
2022-11-22 10:21:53 +0800

    xommit

---
+++ src/tk/amrom/ArithmeticOperation02.java
@@ -0,0 +1,18 @@
+package tk.amrom;
+
+public class ArithmeticOperation02 {
+    public static void main(String[] agrs){
+        int a = 10;
+        a++;
+        System.out.println(a);
+
+        ++a;
+        System.out.println(a);
+
+        a--;
+        System.out.println(a);
+
+        --a;
+        System.out.println(a);
+    }
+}
---
+++ src/tk/amrom/AssignmentOperation01.java
@@ -0,0 +1,16 @@
+package tk.amrom;
+
+public class AssignmentOperation01 {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+
+        a+=b;
+        System.out.println(a);
+        System.out.println(b);
+
+        short s = 1;
+        s += 1;
+        System.out.println(s);
+    }
+}
