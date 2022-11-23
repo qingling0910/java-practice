b5870c4d6ad1ef0c7b314e9fe5fb0cb1b5c3f155
邵庆玲 <shaoqingling@192.168.2.197>
2022-11-23 18:52:38 +0800

    commit

---
+++ src/tk/amrom/WhileTest01.java
@@ -0,0 +1,16 @@
+package tk.amrom;
+
+public class WhileTest01 {
+    public static void main(String[] args) {
+        /*
+        练习：回文数
+        需求：如果 X 是一个回文整数，打印true，否则返回false
+        解释：回文数是指正序（从左到右）和倒序（从右到左）读都是同一个数
+        例如：121是回文数，123不是回文数
+
+         */
+        //定义一个变量记录回文整数初始值
+        int x = 121;
+        //定义一个变量记录回文
+    }
+}
