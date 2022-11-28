1d674a32e319ae4d0c070bb672fbe2e53eeeaa57
shaoqingling172 <qinglingshao172@gmail.com>
2022-11-28 21:52:44 +0800

    commit

---
+++ src/tk/amrom/ArrTest09.java
@@ -0,0 +1,13 @@
+package tk.amrom;
+
+public class ArrTest09 {
+    public static void main(String[] args) {
+        /*
+        练习：打乱数组中的数据
+        需求：定义一个数组，存1～5
+        要求打乱数组中所有的数据的顺序
+         */
+        int[] arr = {1,2,3,4,5};
+
+    }
+}
