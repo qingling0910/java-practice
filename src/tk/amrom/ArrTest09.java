5db2c027987e348e460953799db832e600f21252
shaoqingling172 <qinglingshao172@gmail.com>
2022-11-28 22:32:32 +0800

    comit

--- src/tk/amrom/ArrTest09.java
+++ src/tk/amrom/ArrTest09.java
@@ -1,5 +1,7 @@
 package tk.amrom;
 
+import java.util.Random;
+
 public class ArrTest09 {
     public static void main(String[] args) {
         /*
@@ -8,6 +10,20 @@ public class ArrTest09 {
         要求打乱数组中所有的数据的顺序
          */
         int[] arr = {1,2,3,4,5};
+        //生成随机索引
+        Random r = new Random();
+
+        for(int i = 0; i < arr.length; i++){
+            int Randomindex = r.nextInt(arr.length);
+            int temp = arr[i];
+            arr[i] = arr[Randomindex];
+            arr[Randomindex] = temp;
+
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
 
     }
 }
