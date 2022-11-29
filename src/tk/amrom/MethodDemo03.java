e153d568957e2ac9903187be083a51daf2350c4f
shaoqingling172 <qinglingshao172@gmail.com>
2022-11-29 11:42:04 +0800

    commit

---
+++ src/tk/amrom/MethodDemo03.java
@@ -0,0 +1,17 @@
+package tk.amrom;
+
+public class MethodDemo03 {
+    public static void main(String[] args) {
+        /*
+        目标：利用方法最简单的格式完成当前练习
+        需求：在方法里面定义两个变量并求和打印
+         */
+        getSum();
+    }
+    public static void getSum(){
+        int num1 = 10;
+        int num2 = 20;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
---
+++ src/tk/amrom/MethodDemo04.java
@@ -0,0 +1,13 @@
+package tk.amrom;
+
+public class MethodDemo04 {
+    public static void main(String[] args) {
+        //需求：掌握带参数方法的定义格式和调用格式
+        getSum(10,20);
+
+    }
+    public static void getSum(int num1, int num2){
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
